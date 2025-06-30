5682c592688b2432a06e0b8198c02f408cb70575
xuyq <xuyq@stu.pke.edu.cn>
2025-06-30 09:31:31 +0800

    restart resnet experiment

--- scripts/resnet_train.sh
+++ scripts/resnet_train.sh
@@ -1,5 +1,5 @@
-export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
-opt=("subscafsgd")
+export CUDA_VISIBLE_DEVICES=0,1
+opt=("subscafsgd" "fedavgsgd")
 for opt in "${opt[@]}"
 do
 torchrun --nproc-per-node 2 --master-port 25900 resnet.py \
@@ -7,17 +7,31 @@ torchrun --nproc-per-node 2 --master-port 25900 resnet.py \
     --arch resnet110 \
     --optimizer "${opt}" \
     --batch_size 64 \
-    --tau 10 \
-    --epochs 5 \
+    --tau 5 \
+    --epochs 20 \
     --lr 0.1 \
     --warmup 100 \
     --update_cp_freq 50 \
     --use_log \
     --data_hete \
-    --print-freq 10 \
+    --print-freq 1 \
     --gene_method cd \
     > "./logs/${opt}_resnet_train.log" 2>&1
 done
 
-    #--adaptive_cp_rate 1 \
+torchrun --nproc-per-node 2 --master-port 25900 resnet.py \
+    --comp_dim 7 \
+    --arch resnet110 \
+    --optimizer subscafsgd \
+    --batch_size 64 \
+    --tau 5 \
+    --epochs 20 \
+    --lr 0.1 \
+    --warmup 100 \
+    --update_cp_freq 50 \
+    --use_log \
+    --data_hete \
+    --print-freq 1 \
+    --gene_method cd \
+    > "./logs/subscafsgd_full_resnet_train.log" 2>&1
 
